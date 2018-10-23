d5ff94a26f476e19875b5e8db094271dedf6dcec
saurabhgj70 <saurabh.srivastava@jabong.com>
2018-10-23 17:08:57 +0530

    first

--- App-Test-jenkinsTests/App_Test_jenkinsTests.swift
+++ App-Test-jenkinsTests/App_Test_jenkinsTests.swift
@@ -25,7 +25,7 @@ class App_Test_jenkinsTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         let name = "first"
-        XCTAssertTrue(name == "first", "succescase")
+        XCTAssertTrue(name == "first", "succescas")
         
     }
     
